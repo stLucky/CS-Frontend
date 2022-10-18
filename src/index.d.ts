46004eeb508e5b83876cf3d55ca671a88541848b
stLucky <asys4759@gmail.com>
2022-10-18 18:55:46 +0300

    adds ObjectKeys type for keys method

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,12 @@
 type Nullable<T> = T | null | undefined;
 type CanUndef<T> = T | undefined;
+
+type ObjectKeys<T> =
+  T extends object ? (keyof T)[] :
+    T extends number ? [] :
+      T extends Array<unknown> | string ? string[] :
+        never;
+
+interface ObjectConstructor {
+  keys<T>(o: T): ObjectKeys<T>
+}
