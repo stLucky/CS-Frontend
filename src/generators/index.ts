21264110802460099352e90ba1ae4a16b38af401
stLucky <asys4759@gmail.com>
2022-11-02 13:38:02 +0300

    adds exports and description in the directory

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -1,1 +1,3 @@
 export { default as forEach } from './for-each';
+export { default as numberParser } from './number-parser';
+export { SimpleTaskManager, PriorityTaskManager } from './task-manager';
