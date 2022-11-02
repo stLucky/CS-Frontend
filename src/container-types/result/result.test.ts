c7c5836e32dea267b9ab2246c8181662306f3662
stLucky <asys4759@gmail.com>
2022-11-02 16:00:01 +0300

    refactoring result test

--- src/container-types/result/result.test.ts
+++ src/container-types/result/result.test.ts
@@ -7,30 +7,58 @@ describe('Result', () => {
     result = new Result(() => 10);
   });
 
-  test('should map return a new value', () => {
+  test('should map return a new value', (done) => {
     result
       .map((val) => val * 2)
-      .map((val) => expect(val).toBe(20));
+      .map((val) => {
+        try {
+          expect(val).toBe(20);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
   });
 
-  test('should flatMap unpack the value from the wrapper and return it', () => {
+  test('should flatMap unpack the value from the wrapper and return it', (done) => {
     result
       .flatMap((val) => new Result(() => val * 10))
-      .map((val) => expect(val).toBe(100));
+      .map((val) => {
+        try {
+          expect(val).toBe(100);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
   });
 
-  test('should catch the error with the correct content', () => {
+  test('should catch the error with the correct content', (done) => {
     result
       .map((el) => el * 2)
       .flatMap((el) => Result.error(el))
-      .catch((err) => expect(err).toThrowError('20'));
+      .catch((err) => {
+        try {
+          expect(() => { throw err; }).toThrowError('20');
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
   });
 
-  test('catch should return new data', () => {
+  test('catch should return new data', (done) => {
     result
       .map((el) => Result.error(el))
       .flatMap((el) => Result.error(el))
       .catch(() => 'new data')
-      .map((val) => expect(val).toBe('new data'));
+      .map((val) => {
+        try {
+          expect(val).toBe('new data');
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
   });
 });
